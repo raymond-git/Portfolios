c5405287464841f1a23a91cf4f39a6d59fb90f34
Raymond Huang <raymondhuang615@yahoo.com>
2023-04-04 17:03:46 -0700

    Added Resume Link

--- react-portfolio/src/pages/index.js
+++ react-portfolio/src/pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
             <h1 className='text-xl font-burtons'>myPortfolio</h1>
             <ul className='flex items-center'>
               <li><GiAbstract047 className='cursor-pointer text-2xl' /></li>
-              <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' target="_blank" href="https://publuu.com/flip-book/109878/_0_">Resume</a></li>
+              <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' target="_blank" href="https://publuu.com/flip-book/109878/295287">Resume</a></li>
             </ul>
           </nav>
           <div className='text-center p-10'>
